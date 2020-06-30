7d47c45b7d3767d7b3974586d054fbc2cb983789
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2020-06-30 12:27:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/ratnasrivastava/sounddemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ratnasrivastava.sounddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.bark);
+    }
+
+    public void play(View view) {
+        mediaPlayer.start();
+    }
+
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+}
